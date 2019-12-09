2e69269b2d2dc6740c77f3bb29c5cade4ace4caf
brechtvdv <br5cht@hotmail.com>
2019-12-09 09:16:30 +0100

    Change external_ip to localhost in setup_subscription.sh

--- fill-in-master-ip-address.sh
+++ fill-in-master-ip-address.sh
@@ -5,6 +5,7 @@ KUBE_MASTER_IP=$(kubectl cluster-info | head -n 1 | egrep '[0-9]+\.[0-9]+\.[0-9]
 if [ "$KUBE_MASTER_IP" == "" ]; then
 	# Remove externalIP config
 	sed --in-place "s/- EXTERNAL_IP//g" *.y* setup_ingest.js
+	sed --in-place "s/EXTERNAL_IP:1026/localhost:1026/g" setup_subscription.sh
 else 
 	echo "Kubernetes master's IP address is ${KUBE_MASTER_IP}"
 	sed --in-place "s/EXTERNAL_IP/${KUBE_MASTER_IP}/g" *.y* setup_subscription.sh setup_ingest.js
